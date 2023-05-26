92d537ad0cf7860afeac8f72a2668b76da8729e1
mariomarchan <131277380+mariomarchan@users.noreply.github.com>
2023-05-26 21:16:40 +0200

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,110 @@
+int menu()
+{
+    int opcion;
+
+
+    printf("\n      MENU DE INICIO     \n\n");
+    printf(" Tecnologias:\n\n");
+    printf(" 1.Hidraulica \n 2.Turbinacion bombeo \n 3.Nuclear \n 4.Carbon \n 5. Motores diesel\n 6. Turbina de gas \n 7. Turbina de vapor \n 8. Ciclo combinado \n 9. Hidroeolica \n 10.Eolica \n 11. Solar fotovoltaica \n 12. Solar termica \n 13. Otras renovables \n 14. Cogeneracion \n 15. Residuos no renovables \n 16. Residuos renovables \n 17. Generación total \n");
+    printf(" \n pulse '18' para abandonar el menu\n");
+
+
+    printf("\n Seleccione la tecnologia que desea visualizar:\n");
+    scanf("%s",opcion);
+
+    switch(opcion){
+
+    case 1:
+
+
+        break;
+
+
+    case 2:
+
+        break;
+
+
+    case 3:
+
+        break;
+
+
+    case 4:
+
+        break;
+
+
+    case 5:
+
+        break;
+
+
+    case 6:
+
+        break;
+
+
+    case 7:
+
+        break;
+
+
+    case 8:
+
+        break;
+
+
+    case 9:
+
+        break;
+
+
+    case 10:
+
+        break;
+
+
+    case 11:
+
+        break;
+
+
+    case 12:
+
+        break;
+
+
+    case 13:
+
+        break;
+
+
+    case 14:
+
+        break;
+
+
+    case 15:
+
+        break;
+
+
+    case 16:
+
+        break;
+
+
+    case 17:
+
+        break;
+
+        default:
+                printf("Opcion no valida\n");
+
+    }
+
+
+
+    return menu;
+}
